410be04a905a6fd0f4ecae25ab59a20e67aef313
Alexandre Demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2022-02-16 09:12:44 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'clihm-2022-cc1';
+}
